cb0884bb30bbfae3bfdb3c5fcefb2365fdeeb03f
Zaryab-Programmer <muhammadzaryabrafique@gmail.com>
2022-05-09 17:50:29 +0500

    Add: solana programs and test

--- tests/gifproject.ts
+++ tests/gifproject.ts
@@ -2,15 +2,48 @@ import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { Gifproject } from "../target/types/gifproject";
 
+const { SystemProgram } = anchor.web3;
+
 describe("gifproject", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.Gifproject as Program<Gifproject>;
+  const baseAccount = anchor.web3.Keypair.generate();
 
   it("startStuffOff!", async () => {
     console.log("🚀 Starting test...");
-    const tx = await program.methods.startStuffOff().rpc();
+    let tx = await program.rpc.startStuffOff({
+      accounts: {
+        baseAccount: baseAccount.publicKey,
+        user: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId,
+      },
+      signers: [baseAccount],
+    });
+
     console.log("📝 Your transaction signature", tx);
+  });
+
+  it("Retrieve account data", async () => {
+    let account = await program.account.baseAccount.fetch(
+      baseAccount.publicKey
+    );
+    console.log("👀 GIF Count", account.totalGifs.toString());
+  });
+
+  it("add gif", async () => {
+    await program.rpc.addGif({
+      accounts: {
+        baseAccount: baseAccount.publicKey,
+      },
+    });
+
+    // Get the account again to see what changed.
+    let account = await program.account.baseAccount.fetch(
+      baseAccount.publicKey
+    );
+    console.log("👀 GIF Count", account.totalGifs.toString());
   });
 });
